dfab1d70dc1a7420f678dddaab7e5bc08ae4bb3a
Alex Sierra <asierra@unum.com>
2023-11-26 18:55:39 -0500

    Add photo list UI

--- src/PhotoCapture.tsx
+++ src/PhotoCapture.tsx
@@ -1,7 +1,19 @@
-import { ImageList } from "@mui/material";
+import { ImageList, ImageListItem } from "@mui/material";
 import "./App.css";
+import { useState } from "react";
+
+
 
 function PhotoCapture() {
+  const [images, setImages] = useState<string[]>([]);
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target !== null && e.target.files) {
+        const fileList: FileList = e.target.files
+        setImages(prevList => ([...prevList, URL.createObjectURL(fileList[0])]))
+    }
+  }
+
   return (
       <section id="main-content-container">
         <div className="main-content-photo-header">
@@ -14,12 +26,22 @@ function PhotoCapture() {
             id="photo-capture"
             accept="image/*"
             capture="environment"
+            onChange={handleFileUpload}
           />
         </div>
         <div>
-            <ImageList>
-                
-            </ImageList>
+        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+          {images.map((image, idx) => (
+            <ImageListItem key={image}>
+              <img
+                srcSet={`${image}`}
+                src={`${image}`}
+                alt={`Image of wedding - ${idx}`}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
         </div>
       </section>
   );
